8cbe0c15871ab9dc979034725a79f562cfa4b2dc
Patryk <patkycautomaty@gmail.com>
2026-03-08 11:30:47 +0100

    add Unsuccessful login with invalid credentials test

--- e2e-tests/tests/login.spec.ts
+++ e2e-tests/tests/login.spec.ts
@@ -3,15 +3,30 @@ import { LoginPage } from '../pages/LoginPage';
 
 test.describe('User Authentication', () => {
 
+    let loginPage: LoginPage;
+
+    test.beforeEach(async ({ page }) => {
+        loginPage = new LoginPage(page);
+        await loginPage.blockAds();
+    });
+
     test('Successful login with valid credetials', async ({ page }) => {
-        const loginPage = new LoginPage(page);
         
         await loginPage.navigate();
         await loginPage.acceptCookies();
-        await loginPage.blockAds();
         await loginPage.performLogin("testowymail11231123@mailowy.com","testhaslo123");
 
         const loggedInTest = page.getByText("Logged in as");
         await expect(loggedInTest).toBeVisible();
+        });
+    
+    test('Unsuccessful login with invalid credentials', async ({ page }) => {
+        await loginPage.navigate()
+        await loginPage.acceptCookies();
+        await loginPage.performLogin("testowymail11231123@mailowy.com","testhaslo1234");
+
+        const incorrectLoginText = page.getByText("Your email or password is incorrect!");
+        await expect(incorrectLoginText).toBeVisible();
+
+        })
     });
-});
